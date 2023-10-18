235740ec96f864f728644396b69455cc7c1fbf99
Mike Decker <pookmish@gmail.com>
2023-10-18 13:28:16 -0700

    Updated css compilers

--- themes/diversityworks/webpack.config.js
+++ themes/diversityworks/webpack.config.js
@@ -1,4 +1,10 @@
+
 const path = require("path");
+const Webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 const config = {
   isProd: process.env.NODE_ENV === "production",
@@ -8,11 +14,6 @@ const config = {
   wdsPort: 3001,
 };
 
-const Webpack = require("webpack");
-const AssetsWebpackPlugin = require('assets-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-
 var webpackConfig = {
   entry: {
     "diversityworks.styles": "./lib/scss/diversityworks.scss"
@@ -20,12 +21,27 @@ var webpackConfig = {
   output: {
     path: config.distFolder,
     filename: '[name].js',
-    publicPath: config.publicPath,
-    clean: true
+    assetModuleFilename: '../assets/[name][ext][query]'
   },
   mode: config.isProd ? "production" : "development",
+  resolve: {
+    alias: {
+      'decanter-assets': path.resolve('node_modules', 'decanter/core/src/img'),
+      'decanter-src': path.resolve('node_modules', 'decanter/core/src'),
+      '@fortawesome': path.resolve('node_modules', '@fortawesome'),
+      'fa-fonts': path.resolve('node_modules', '@fortawesome/fontawesome-free/webfonts')
+    }
+  },
   module: {
     rules: [
+      {
+        test: /\.behavior.js$/,
+        exclude: /node_modules/,
+        options: {
+          enableHmr: false
+        },
+        loader: 'drupal-behaviors-loader'
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
@@ -40,17 +56,35 @@ var webpackConfig = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           config.isProd ? { loader: MiniCssExtractPlugin.loader } : 'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ],
+          {loader:'css-loader', options: {}},
+          {loader:'postcss-loader', options: {}},
+          {loader:'sass-loader', options: {}}
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/i,
+        type: "asset",
+        generator: {
+          filename: '../assets/fonts/[name][ext][query]'
+        }
       }
     ]
   },
   plugins: [
-    new AssetsWebpackPlugin({path: config.distFolder}),
+    new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].css',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"]
+        }
+      }
     }),
   ],
   optimization: {
@@ -58,7 +92,6 @@ var webpackConfig = {
       new OptimizeCSSAssetsPlugin(),
     ]
   }
-
 };
 
 if (config.hmrEnabled) {
