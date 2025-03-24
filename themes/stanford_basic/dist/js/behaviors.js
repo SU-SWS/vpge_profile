d67a5ea22583cb12e91b2e972eb693ba36bb5429
mdyoung3 <mdyoung3@stanford.edu>
2025-03-24 15:11:58 -0600

    Ran theme updates

--- themes/stanford_basic/dist/js/behaviors.js
+++ themes/stanford_basic/dist/js/behaviors.js
@@ -135,7 +135,7 @@ window.Drupal.behaviors.stanford_basic = {
        */
       $('.topics__collapsable-menu', context).click(function () {
         $(this).toggleClass('show');
-        if ($(this).siblings('.menu').css('display') != 'none') {
+        if ($(this).siblings('.menu').css('display') !== 'none') {
           $(this).attr('aria-expanded', 'true');
         } else {
           $(this).attr('aria-expanded', 'false');
@@ -176,40 +176,6 @@ window.Drupal.behaviors.stanford_basic = {
     // console.log("Detached.");
   }
 };
-
-/***/ }),
-
-/***/ 8035:
-/***/ (function() {
-
-var header = document.getElementById('block-stanford-basic-local-tasks');
-var sticky = 0;
-if (header) {
-  sticky = header.getBoundingClientRect().top;
-  window.onscroll = function () {
-    stickyHeaderOnScroll();
-  };
-}
-
-/**
- * Stick the local block tasks to the top of the window.
- */
-function stickyHeaderOnScroll() {
-  var toolbarHeight = 0;
-  var toolbarOpen = document.body.classList.contains('toolbar-tray-open');
-  if (toolbarOpen === true) {
-    toolbarHeight = 79;
-  } else {
-    toolbarHeight = 39;
-  }
-  if (window.pageYOffset >= sticky - toolbarHeight) {
-    header.classList.add('sticky');
-    header.style.marginTop = toolbarHeight + 'px';
-  } else {
-    header.classList.remove('sticky');
-    header.style.marginTop = '0px';
-  }
-}
 
 /***/ })
 
