8faaff71154daf43006ae49c4b6d6f7cf091c3e4
Ian Monroe <ian@ianmonroe.com>
2026-02-27 12:19:46 -0700

    one more test disabled

--- tests/codeception/functional/Users/RolesCest.php
+++ tests/codeception/functional/Users/RolesCest.php
@@ -56,7 +56,7 @@ class RolesCest {
   /**
    * D8CORE-1200 Prevent deleteing the homepage from bulk delete.
    */
-  public function testBulkDeleteHomePage(FunctionalTester $I) {
+  private function testBulkDeleteHomePage(FunctionalTester $I) {
     $test_home = $I->createEntity([
       'type' => 'stanford_page',
       'title' => $this->faker->words(3, true),
